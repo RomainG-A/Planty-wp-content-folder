17530d17ba9355a2d092f765fd6f81005a9b8212
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2022-11-04 15:35:15 +0100

    header with custom commander

--- themes/blankslate-child/header.php
+++ themes/blankslate-child/header.php
@@ -14,7 +14,6 @@
         </div>
         <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement"> 
             <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-            <div id="search"><?php get_search_form(); ?></div>
         </nav>
     </header>
 <div id="container">
