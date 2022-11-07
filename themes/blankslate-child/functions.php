aa6e26b2876fc2b03ec1e6013675552f2bf93ab3
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2022-11-07 19:17:24 +0100

    menu responsif v1
    une version fonctionnelle mais qui pourrait etre un peu plus jolie

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -27,8 +27,3 @@ function extra_setup() {
     register_nav_menu ('primary mobile', __( 'Navigation Mobile', 'twentythirteen' ));
 }
 add_action( 'after_setup_theme', 'extra_setup' );
-
-/*function set_container_class ($args) {
-    $args['container_class'] = str_replace(' ','-',$args['theme_location']).'-nav'; return $args;
-    }
-add_filter ('wp_nav_menu_args', 'set_container_class');*/
